4773033aabb7f108d27a4d271128929e7b691e36
JuanRoccia <juanroccia@gmail.com>
2024-03-20 20:55:55 -0300

    funtions.php & style.min.css

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -8,7 +8,6 @@ add_action('wp_enqueue_scripts', 'mychildtheme_enqueue_styles');
 function psw_custom_scripts_cdns() {
     ?>
 	<link rel="stylesheet" href="http://localhost/la_casa_del_tapicero/wp-content/themes/astra-child/style.min.css" type="text/css"/>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <?php
 }
 add_action('wp_head', 'psw_custom_scripts_cdns');
