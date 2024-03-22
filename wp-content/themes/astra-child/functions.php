df98cff916b1028f42decd63160ec0ebf0e9331e
JuanRoccia <juanroccia@gmail.com>
2024-03-22 19:12:44 -0300

    funtions.php fixed & style.min.css

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -2,5 +2,7 @@
 function mychildtheme_enqueue_styles() {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('child-style-min', get_stylesheet_directory_uri() . '/style.min.css', array('parent-style'), wp_get_theme()->get('Version'));
+    wp_enqueue_script('child-script', get_stylesheet_directory_uri() . '/script.js', array('jquery'), wp_get_theme()->get('Version'), true);
+    wp_enqueue_script('child-script-min', get_stylesheet_directory_uri() . '/script.min.js', array('jquery'), wp_get_theme()->get('Version'), true);
 }
 add_action('wp_enqueue_scripts', 'mychildtheme_enqueue_styles');
